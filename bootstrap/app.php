f55f2fed61e56a2157e1c95f8799924a16c43d54
umaarov <hs.umarov21@gmail.com>
2025-05-05 11:22:41 +0500

    refactor: enable API routing in application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-//        api: __DIR__ . '/../routes/api.php',
+//        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
